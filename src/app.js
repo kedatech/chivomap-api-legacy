886dbc0207594e80c86523bd35f680406d03b40b
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-07-14 03:33:50 -0600

    chore: :sparkles: config type module and app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 import express from 'express'
+import cors from 'cors'
 
-const app = express()
-app.use(app.json())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//middlewar
+const whitelist = ['http://localhost:3000', 'https://chivomap.vercel.app'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      console.log('forbidden')
+      // todo: callback(new Error(boom.forbidden('Access denied due to CORS security restrictions')));
+    }
+  }
+}
+app.use(cors(options));
 
 export default app
