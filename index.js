bb5e8b4d01f98006869bce4f1a1eb14a892cef1d
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2024-05-26 10:51:09 -0600

    feat: :sparkles: add envs

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import app from './src/app.js'
+import { config } from 'dotenv'
 
-const PORT = 3002
+config()
+
+const PORT = process.env.PORT || 3000
+
 app.listen(PORT, ()=>{
   console.log(`Server running on http://localhost:${PORT}`)
 })
