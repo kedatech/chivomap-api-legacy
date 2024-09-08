5aad9711e004d704d3c3c1fcfc2dbf4882b4c00f
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2024-09-08 13:59:10 -0600

    fix: :bug: geo json feature response

--- src/services/geo.services.js
+++ src/services/geo.services.js
@@ -1,29 +1,38 @@
-import { readFile } from 'fs/promises'
+import { readFile } from 'fs/promises';
 import topojson from "topojson-client";
+
+// Leer el archivo topo.json
 const file = await readFile('./assets/topo.json', 'utf-8');
 const data = JSON.parse(file);
 const { feature } = topojson;
 
 // Función para obtener los municipios de un departamento
 export const getMunicipios = (query, whatIs) => {
-    // whatIs solo puede ser 'departamento' o 'municipio' o 'distrito'
-    if (!whatIs || (whatIs !== 'D' && whatIs !== 'M' && whatIs !== 'NAM')) {
-        throw new Error('El segundo parámetro debe ser "M" o "D" o "NAM"');
-    }
+  // Validar whatIs
+  if (!whatIs || (whatIs !== 'D' && whatIs !== 'M' && whatIs !== 'NAM')) {
+    throw new Error('El segundo parámetro debe ser "M" o "D" o "NAM"');
+  }
 
-    if (!data?.objects?.collection) {
-        throw new Error('No se encontraron datos válidos');
-    }
+  // Validar que los datos existan
+  if (!data?.objects?.collection) {
+    throw new Error('No se encontraron datos válidos');
+  }
 
-    const geojson = feature(data, data.objects.collection);
-  
-    // Filtrar los municipios correspondientes al departamento especificado
-    const municipios = geojson.features.filter (item => { 
-        return item.properties[whatIs] === query;
-    });
+  // Convertir topojson a GeoJSON
+  const geojson = feature(data, data.objects.collection);
 
-    return municipios;
-}
+  // Filtrar los municipios correspondientes al query y whatIs
+  const municipios = geojson.features.filter(item => {
+    return item.properties[whatIs] === query;
+  });
+
+  // Devolver un FeatureCollection
+  return {
+    type: "FeatureCollection",
+    features: municipios // Aseguramos que sea un array de features
+  };
+};
+
 
 
 // función para obtener un objeto con nombres de { departamentos[], municipios[], distritos[] }
