79c8038ce3903702ac7573091142d431c8d2f408
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2024-09-08 12:07:59 -0600

    fix: allow all origin - cors

--- src/app.js
+++ src/app.js
@@ -3,21 +3,24 @@ import cors from 'cors';
 import routes from './routes/index.js';
 
 const app = express();
-const whitelist = ['http://localhost:3000', 'https://chivomap.vercel.app', 'https://chivomap.com'];
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Access denied due to CORS security restrictions'));
-    }
-  }
-};
+// const whitelist = ['http://localhost:3000', 'https://chivomap.vercel.app', 'https://chivomap.com'];
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (whitelist.includes(origin) || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Access denied due to CORS security restrictions'));
+//     }
+//   }
+// };
 
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+// permitir a todos los origenes
+app.use(cors());
 
 // Rutas
 app.use('/api', routes);
